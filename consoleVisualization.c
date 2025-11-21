d39ba5e247b016e4f463cf34edfcfb591e286139
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-11-21 18:53:32 +0100

    Add detailed description for `showBoard` function.

--- consoleVisualization.c
+++ consoleVisualization.c
@@ -1,9 +1,18 @@
 //
 // Created by theVo on 19/11/2025.
-//
 
 #include "consoleVisualization.h"
 
+/**
+ * @brief Displays the current state of the game board.
+ *
+ * This function prints a textual representation of the board to
+ * standard output. It is called during both the placement and movement
+ * phases to provide the player with a visual overview of penguins,
+ * fish, and empty tiles.
+ *
+ * @return void
+ */
 void showBoard() {
     printf("Displaying the board.\n");
 }
