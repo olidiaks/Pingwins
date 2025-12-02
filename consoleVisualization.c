37826b4a184be689949e39992123449133b1204d
TylerShax <247200418+TylerShax@users.noreply.github.com>
2025-12-02 13:45:04 +0100

    Added signatures to rows and columns Board now displays in a grid Board size can now be bigger thanks to indexing of the signatures (A1, B1, etc)

--- consoleVisualization.c
+++ consoleVisualization.c
@@ -6,10 +6,66 @@
 
 void showBoard(struct GameState *gameState) {
     printf("Displaying the board.\n");
-    for (int x = 0; x < gameState->x_Board_size; x++) {
-        for (int y = 0; y < gameState->y_Board_size; y++) {
-            printf("%d ", gameState->Board[x][y].amount_of_fish);
+
+    char horChr, vertChr;
+    unsigned short int horMul, vertMul;
+
+    for (int x = 0; x <= gameState->x_Board_size; x++) {
+        for (int y = 0; y <= gameState->y_Board_size; y++) {
+
+            horChr = 64 + y%26;
+            horMul = y / 26 + 1;
+
+            vertChr = 64 + 32 + x%26;
+            vertMul = x / 26 + 1;
+
+            if (x == 0 && y == 0) {
+                printf("\t");
+            }
+
+            if (x == 0 && y != 0)  {
+                printf("%c%d\t", horChr, horMul);
+            }
+            else if (y == 0 && x != 0) {
+                printf("%c%d\t", vertChr, vertMul);
+            }
+            else if (x != 0 && y != 0) {
+                //if (y > 1) {
+                    printf("%d   |\t", gameState->Board[x-1][y-1].amount_of_fish);
+                //}
+                //else {
+                //    printf("%d\t", gameState->Board[x-1][y-1].amount_of_fish);
+                //}
+
+            }
+        }
+        printf("\n\t");
+        for (int x = 0; x < gameState->x_Board_size; x++) {
+            printf("========");
         }
         printf("\n");
     }
 }
+
+
+
+// if (x==0) {
+//     if (y==0) {
+//         printf(" \t");
+//     }
+//     else {
+//         printf("%c%d\t", c, extra);
+//     }
+// }
+// else {
+//     if (y==0) {
+//         if (x == 1) {
+//             c = 65 + 32;
+//         }
+//
+//         printf("%c%d\t", c, extra);
+//     }
+//     else {
+//         printf("%d\t", gameState->Board[x-1][y-1].amount_of_fish);
+//     }
+// }
