2913b3e8842f09b209003195a7a9ddeed9109e62
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-12-02 10:48:21 +0100

    "Display board with fish amounts in `showBoard` function."

--- consoleVisualization.c
+++ consoleVisualization.c
@@ -6,4 +6,10 @@
 
 void showBoard(struct GameState *gameState) {
     printf("Displaying the board.\n");
+    for (int x = 0; x < gameState->x_Board_size; x++) {
+        for (int y = 0; y < gameState->y_Board_size; y++) {
+            printf("%d ", gameState->Board[x][y].amount_of_fish);
+        }
+        printf("\n");
+    }
 }
