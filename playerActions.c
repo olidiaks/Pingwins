537bc8a8a135ba1c0f09d0a27191eaeffbcd96a6
TylerShax <247200418+TylerShax@users.noreply.github.com>
2025-12-02 14:36:16 +0100

    askCoordinates reads two variables x and y

--- playerActions.c
+++ playerActions.c
@@ -15,8 +15,9 @@ void changeCurrentPlayer(struct GameState *gameState) {
 }
 
 void askCoordinates(struct GameState *gameState) {
+    int x,y;
     printf("Player %d, please input coordinates (row, column): \n",1+gameState->current_player);
-    while (scanf("%s %s", &gameState->Players->x, &gameState->Players->y) != 2 || !isMoveValid()) {
+    while (scanf("%s %s",x, y) != 2 || !isMoveValid()) {
         printf("Please provide correct values!\n");
         while (getchar() != '\n');
 
