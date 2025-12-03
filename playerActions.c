52e1d93d46bb1da03437c2c335b801cf0645f04f
realjukarts <realjukarts@gmail.com>
2025-12-03 17:58:23 +0100

    Fixed functions in playerActions.c

--- playerActions.c
+++ playerActions.c
@@ -14,29 +14,30 @@ void changeCurrentPlayer(struct GameState *gameState) {
 }
 
 void askCoordinates(struct GameState *gameState) {
-    char x[2], y[2];
-    printf("Player %d, please input coordinates (row, column): \n", 1 + gameState->current_player);
-    while (scanf("%s %s", &x, &y) != 2 || !isMoveValid(x, y, gameState)) {
+    char x[50], y[50];
+    printf("Player %d, please input coordinates seperated with a space (row, column): \n", 1 + gameState->current_player);
+    // while (scanf("%s %s", &x, &y) != 2 || !isMoveValid(x, y, gameState)) { // For now we assume that every movement is valid.
+    while (scanf("%s %s", &x, &y) != 2) {
         printf("Please provide correct values!\n");
         while (getchar() != '\n');
     }
 }
 
-bool isMoveValid(char x[2], char y[2], struct GameState *gameState) {
+bool isMoveValid(char x[50], char y[50], struct GameState *gameState) {
 
-    printf("%s %s \n",x,y);
+    // printf("%s %s \n",x,y);
     
     char x_FirstChar = toupper(x[0]);
     char x_SecondChar = toupper(x[1]);
     char y_FirstChar = toupper(y[0]);
     char y_SecondChar = toupper(y[1]);
 
-    printf("%c %c %c %c \n",x_FirstChar,x_SecondChar,y_FirstChar,y_SecondChar);
+    // printf("%c %c %c %c \n",x_FirstChar,x_SecondChar,y_FirstChar,y_SecondChar);
 
     int xo = (int) (x_FirstChar - 65) + (atoi(&x_SecondChar) - 1) * 26;
-    int yo = (int) (y_FirstChar - 65) + (atoi(&y_SecondChar) - 1) * 26;
+    int yo = (int) (y_FirstChar - 65) + (atoi(&y_SecondChar) - 1) * 26 - 260;
 
     printf("X: %d, Y: %d \n", xo, yo);
 
-    return 0 < xo && xo < gameState->x_Board_size && 0 < yo && yo < gameState->y_Board_size;
+    return (0 > xo) && (xo > gameState->x_Board_size) && (0 > yo) && (yo > gameState->y_Board_size);
 }
