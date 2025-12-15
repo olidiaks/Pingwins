1cf601a66c584fef036a51c65cdb3c9e369b4616
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-12-15 19:11:56 +0100

    Refactor `placement.c`: fix inconsistent formatting for improved readability and maintainability.

--- placement.c
+++ placement.c
@@ -5,10 +5,12 @@
 #include "placement.h"
 
 
-void placementPhase(struct GameState *gameState) {
+void placementPhase(struct GameState* gameState)
+{
     readBoardDimensions(gameState);
     generateBoard(gameState);
-    while (canPlayerPlacePenguin(gameState)) {
+    while (canPlayerPlacePenguin(gameState))
+    {
         showBoard(gameState);
         askCoordinates(gameState);
         while (!isCoordinateValid(gameState) || !isPlacemntVaild(gameState))
@@ -24,12 +26,16 @@ void placementPhase(struct GameState *gameState) {
     }
 }
 
-bool canPlayerPlacePenguin(struct GameState *gameState) {
+bool canPlayerPlacePenguin(struct GameState* gameState)
+{
     int counter = 0;
-    for (int i = 0; i < gameState->x_Board_size; i++) {
-        for (int j = 0; j < gameState->y_Board_size; j++) {
+    for (int i = 0; i < gameState->x_Board_size; i++)
+    {
+        for (int j = 0; j < gameState->y_Board_size; j++)
+        {
             // printf("%d\n",gameState->Board[i][j].amount_of_fish);
-            if (gameState->Board[i][j].amount_of_fish == 0) {
+            if (gameState->Board[i][j].amount_of_fish == 0)
+            {
                 counter++;
             }
         }
@@ -37,27 +43,31 @@ bool canPlayerPlacePenguin(struct GameState *gameState) {
     // printf("spots with 0 %d \n",counter);
     // printf("%d %d \n",gameState->x_Board_size,gameState->y_Board_size);
     return counter != gameState->x_Board_size * gameState->y_Board_size && counter != gameState->num_of_players *
-           gameState->num_of_penguins_per_player;
+        gameState->num_of_penguins_per_player;
 }
 
 
-bool isEveryPenguinsPlaced(struct GameState *gameState) {
-    for (int i = 0; i < gameState->num_of_players; ++i) {
-        if (gameState->Players[i].current_penguin != gameState->num_of_penguins_per_player) {
+bool isEveryPenguinsPlaced(struct GameState* gameState)
+{
+    for (int i = 0; i < gameState->num_of_players; ++i)
+    {
+        if (gameState->Players[i].current_penguin != gameState->num_of_penguins_per_player)
+        {
             return false;
         }
     }
     return true;
 }
 
-bool isPlacemntVaild(struct GameState *gameState) {
+bool isPlacemntVaild(struct GameState* gameState)
+{
     int x = gameState->Players[gameState->current_player].penguins->x;
     int y = gameState->Players[gameState->current_player].penguins->y;
     return gameState->Board[x - 1][y - 1].amount_of_fish == 1;
 }
 
-void placePenguin(struct GameState *gameState) {
+void placePenguin(struct GameState* gameState)
+{
     printf("Player %d 's penguin has been placed.\n", gameState->current_player + 1);
     change_penguin_position(gameState);
 }
-
