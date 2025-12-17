8bc1e6ee54440f596817b2f30837ff4da41be0c6
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-12-17 13:18:11 +0100

    Refactor player action functions: improve formatting, add default handling in `askCoordinates`, and ensure consistent code style.

--- playerActions.c
+++ playerActions.c
@@ -4,40 +4,33 @@
 
 #include "playerActions.h"
 
-void collectFish(struct GameState* gameState)
-{
+void collectFish(struct GameState *gameState) {
     int x = gameState->Players[gameState->currentPlayer].penguins->x;
     int y = gameState->Players[gameState->currentPlayer].penguins->y;
-    printf("x = %d, y = %d \n",x,y);
+    printf("x = %d, y = %d \n", x, y);
     gameState->Players[gameState->currentPlayer].currentScore += gameState->Board[x][y].amountOfFish;
     gameState->Board[x][y].amountOfFish = 0;
     printf("Player %d has collected fish.\n", gameState->currentPlayer + 1);
 }
 
-void changeCurrentPlayer(struct GameState* gameState)
-{
+void changeCurrentPlayer(struct GameState *gameState) {
     gameState->currentPlayer = (gameState->currentPlayer + 1) % gameState->numOfPlayers;
     printf("Current player is player %d.\n", 1 + gameState->currentPlayer);
 }
 
-void askCoordinates(struct GameState* gameState)
-{
+void askCoordinates(struct GameState *gameState) {
     printf("Please input coordinates (row column): \n");
 
-    char* x = NULL;
-    char* y = NULL;
+    char *x = NULL;
+    char *y = NULL;
 
-    unsigned short lenX;
-    unsigned short lenY;
+    unsigned short lenX = 0;
+    unsigned short lenY = 0;
 
-    if (scanf("%ms %ms", &x, &y) == 2)
-    {
+    if (scanf("%ms %ms", &x, &y) == 2) {
         lenX = strlen(x);
         lenY = strlen(y);
-    }
-
-    if (lenX == 2 && lenY == 2)
-    {
+    } else if (lenX == 2 && lenY == 2) {
         int xOffset = toupper(x[0]);
         int xDirect = toupper(x[1]);
         int yOffset = toupper(y[0]);
@@ -49,11 +42,15 @@ void askCoordinates(struct GameState* gameState)
 
         gameState->Players[gameState->currentPlayer].x = xFinal - 1;
         gameState->Players[gameState->currentPlayer].y = yFinal - 1;
+    } else {
+        printf("Check once again how cordinates of that field are called.\n");
+
+        gameState->Players[gameState->currentPlayer].x = -1;
+        gameState->Players[gameState->currentPlayer].y = -1;
     }
 }
 
-bool isCoordinateValid(struct GameState* gameState)
-{
+bool isCoordinateValid(struct GameState *gameState) {
     int x = gameState->Players[gameState->currentPlayer].x;
     int y = gameState->Players[gameState->currentPlayer].y;
 
@@ -72,14 +69,14 @@ bool isMoveValid(struct GameState *gameState) {
     moveX = gameState->Players[curPlr].x;
     moveY = gameState->Players[curPlr].y;
 
-    printf("cur X = %d cur Y = %d move X = %d move Y = %d\n",curX,curY,moveX,moveY);
+    printf("cur X = %d cur Y = %d move X = %d move Y = %d\n", curX, curY, moveX, moveY);
 
     int deltaX, deltaY, dt;
 
     deltaX = abs(moveX - curX);
     deltaY = abs(moveY - curY);
 
-    printf("delta x = %d delta y = %d\n",deltaX,deltaY);
+    printf("delta x = %d delta y = %d\n", deltaX, deltaY);
     printf("%d \n", gameState->Board[moveY, moveX]->amountOfFish);
 
     if (gameState->Board[moveY, moveX]->amountOfFish == 0) {
@@ -97,14 +94,13 @@ bool isMoveValid(struct GameState *gameState) {
     printf("Player didn't input an invalid move\n");
 
     if (deltaX != 0) {
-
         int iterator = 0;
         int cPosX, cPosY = curX, curY;
         int sign = deltaX / abs(deltaX);
 
         if (iterator > deltaX) {
             iterator = deltaX;
-            deltaX = 0;         //We perform this fantastic manouver to make sure for if goes lower -> bigger
+            deltaX = 0; //We perform this fantastic manouver to make sure for if goes lower -> bigger
 
             for (iterator; iterator <= deltaX; iterator++) {
                 cPosX += sign;
@@ -116,15 +112,14 @@ bool isMoveValid(struct GameState *gameState) {
 
             return true;
         }
-    }
-    else {
+    } else {
         int iterator = 0;
         int cPosX, cPosY = curX, curY;
         int sign = deltaY / abs(deltaY);
 
         if (iterator > deltaY) {
             iterator = deltaY;
-            deltaY = 0;         //We perform this fantastic manouver to make sure for if goes lower -> bigger
+            deltaY = 0; //We perform this fantastic manouver to make sure for if goes lower -> bigger
 
             for (iterator; iterator <= deltaY; iterator++) {
                 cPosY += sign;
@@ -137,7 +132,6 @@ bool isMoveValid(struct GameState *gameState) {
             return true;
         }
     }
-
 }
 
 void changePenguinPosition(struct GameState *gameState) {
@@ -153,6 +147,6 @@ void changePenguinPosition(struct GameState *gameState) {
     gameState->Board[x][y].idPenguin = gameState->Players[curPlr].currentPenguin;
 }
 
-void changeCurrentPenguin(struct GameState* gameState) {
+void changeCurrentPenguin(struct GameState *gameState) {
     gameState->Players[gameState->currentPlayer].currentPenguin++;
 }
