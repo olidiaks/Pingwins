4926522d99ed08ef145ab4197a998db8cb09dd61
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-12-18 02:54:51 +0100

    Add `placePenguinAutomaticli` function for automated penguin placement and update related documentation

--- autonomusPlacement.c
+++ autonomusPlacement.c
@@ -5,6 +5,7 @@
 #include "autonomusPlacement.h"
 
 #include "autonomusMovement.h"
+#include "placementInteractive.h"
 
 void autonomousPlacement(struct GameState *gameState, char inputFilePath[], char outputFilePath[], char nameOfUs[]) {
     FILE *inputFile = openInputFileAndHandleError(inputFilePath);
@@ -18,7 +19,17 @@ void autonomousPlacement(struct GameState *gameState, char inputFilePath[], char
 
     fclose(inputFile);
 
+    placePenguinAutomaticli(gameState);
+
     FILE *outputFile = openOutputFileAndHandleError(outputFilePath);
     writeBoardToFile(outputFile, gameState);
     fclose(outputFile);
+}
+void placePenguinAutomaticli(struct GameState *gameState) {
+    do {
+        gameState->Players[gameState->currentPlayer].x = rand() % gameState->xBoardSize;
+        gameState->Players[gameState->currentPlayer].y = rand() % gameState->yBoardSize;
+    } while (!(isCoordinateValid(gameState) && isPlacementValid(gameState)));
+    placePenguin(gameState);
+    collectFish(gameState);
 }
