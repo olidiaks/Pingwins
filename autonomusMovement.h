0fd530e14919be4f62e11212836c0cd5465c8130
TylerShax <247200418+TylerShax@users.noreply.github.com>
2025-12-18 01:36:51 +0100

    Included reconstructing a file (UNTESTED!!!! can't check - other scripts broken ;( ) + marked what's done on Work Tommorow + implemented reconstruction into autonomous movement.

--- autonomusMovement.h
+++ autonomusMovement.h
@@ -6,7 +6,9 @@
 #define PROJECT_AUTONOMUSMOVEMENT_H
 
 #include "autonomousMode.h"
+#include  "GameState.h"
 
+void writeBoardToFile(FILE *outputFile, struct GameState *gameState);
 
 void autonomousMovement(struct GameState *gameState, char inputFilePath[], char outputFilePath[], char nameOfUs[]);
 
