9a07c5b3b7c679b55fd331a74ebeebabcec5d5cd
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-12-18 11:49:21 +0100

    Add error handling for non-incremental player ID assignment in `autonomousMode`

--- autonomousMode.c
+++ autonomousMode.c
@@ -142,11 +142,16 @@ bool loadPlayers(struct GameState *game_state, FILE *input_file) {
             exit(2);
         }
 
+
         int idx = id - 1;
         game_state->Players[idx].name = name;
         game_state->Players[idx].currentScore = score;
         occupiedIDs[idx] = 1;
         game_state->numOfPlayers++;
+        if (idx > game_state->numOfPlayers) {
+            printf("Id's of player need to be filled incrementali and for layer %s, that do not happend.\n", name);
+            exit(2);
+        }
 
         if (strcmp(name, game_state->teamName) == 0) {
             game_state->currentPlayer = idx;
