f5dc8fd5ef7a83c95e358636e1c0a9e7e88958f1
Oliwier Olszewski <oliwierolszewski@proton.me>
2025-12-22 11:49:03 +0100

    Update documentation for `interactive_mode` function in `interactiveMode.h`

--- interactiveMode.h
+++ interactiveMode.h
@@ -12,12 +12,17 @@
 #include <stdlib.h>
 
 /**
- * Handles the interactive mode of the game, including initialization, placement phase,
- * movement phase, and displaying the game board. The function also determines the winner
- * based on the final scores of the players once the movement phase is completed.
+ * Handles the interactive mode of the game.
  *
- * @param gameState Pointer to the GameState structure that holds the current state of the game,
- * including the board, players, penguins, and other game-related data.
+ * This function initializes the game environment, facilitates the placement
+ * of penguins on the game board, and manages the movement phase of the game
+ * interactively. It also determines the result of the game and announces
+ * the winner at the end. If penguins are not properly placed, the game will
+ * terminate with an error.
+ *
+ * @param gameState Pointer to the GameState structure that contains the
+ *        current state of the game, including player data, board configuration,
+ *        and game settings.
  */
 void interactive_mode(struct GameState *gameState);
 
