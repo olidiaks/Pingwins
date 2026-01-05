294d9e73af7c6559f09b93b66d55e90fd0dad1fd
Oliwier Olszewski <oliwierolszewski+code@proton.me>
2026-01-05 17:14:39 +0100

    Replace redundant local variable `name` with direct usage of `gameState.teamName` and include `GameState.h` in `main.h`.

--- main.c
+++ main.c
@@ -14,19 +14,18 @@ int main(int argc, char *argv[]) {
     srand(time(&tt));
 
     gameState.teamName = "Pingwiniarze";
-    char *name = gameState.teamName;
 
     switch (argc) {
         case 1: interactiveMode(&gameState);
             break;
         case 2:
-            printf("%s", name);
+            printf("%s", gameState.teamName);
             break;
         case 4:
-            autonomousMovement(&gameState, argv[2], argv[3], name);
+            autonomousMovement(&gameState, argv[2], argv[3], gameState.teamName);
             break;
         case 5:
-            autonomousPlacement(&gameState, argv[3], argv[4], name, argv[2]);
+            autonomousPlacement(&gameState, argv[3], argv[4], gameState.teamName, argv[2]);
             break;
         default: printf("Too many arguments!\nAmount of given arguments: %d\n", argc);
             exit(5);
