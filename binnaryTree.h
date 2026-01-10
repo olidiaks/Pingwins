7acc437776c1f4ff864f13ae950cd8b5614722bc
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-01-10 02:56:44 +0100

    Add detailed documentation for `createNode` function in `binnaryTree.h`

--- binnaryTree.h
+++ binnaryTree.h
@@ -17,6 +17,18 @@ struct Node *insertNode(struct Node *root, int *value);
 
 void searchNode(struct Node *treeBranch, int *value);
 
+/**
+ * @brief Creates a new node for a binary tree.
+ *
+ * This function allocates memory for a new binary tree node,
+ * initializes its data with the provided value, and sets its
+ * left and right child pointers to NULL. If memory allocation
+ * fails, the function will print an error message and terminate
+ * the program.
+ *
+ * @param value Pointer to the integer value to be assigned to the node.
+ * @return Pointer to the newly created Node structure.
+ */
 struct Node *createNode(int *value);
 
 #endif // PROJECT_BINNARYTREE_H
