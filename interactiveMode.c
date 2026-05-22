a8f991620874707da710ac88704b53b79c5b839f
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-05-22 15:59:40 +0200

    Add player score logs to `show_winner_announcement` for better transparency

--- interactiveMode.c
+++ interactiveMode.c
@@ -20,16 +20,16 @@ void interactiveMode(struct GameState *gameState) {
 }
 
 void show_winner_announcement(struct GameState *gameState) {
-    printf("- - - - - - - - - - Announcing winner!!! - - - - - - - - - -\n\n");
+    printf("\n - - - - - - - - - - Announcing winner!!! - - - - - - - - - -\n\n");
 
     bool isTie = false;
     int winningPlayer = 0;
     int winningFish = 0;
     for (int i = 0; i < gameState->numOfPlayers; i++) {
         int current_score = gameState->Players[i].currentScore;
+        printf("Player %d has %d\n", i + 1, current_score);
         if (current_score > winningFish) {
             winningFish = current_score;
-            printf("Player %d has %d", i + 1, current_score);
             isTie = false;
         }
         else {
